01efb2840215c9e0611148a1b3463aef42a6fd5b
taleore <taleore@gmail.com>
2019-09-04 11:46:21 +0200

    add funtcion

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,1 +1,90 @@
 "use strict";
+
+const selectNumber = document.querySelectorAll(".radioNum");
+const btnStart = document.querySelector(".btn-start");
+let cardImage = [];
+const urlImage =
+  "https://via.placeholder.com/160x195/30d9c4/ffffff/?text=ADALAB.";
+
+//const cardList = document.querySelector(".showCards__list");
+
+function numberCard() {
+  //returns number of cards
+  for (let i = 0; i < selectNumber.length; i++) {
+    if (selectNumber[i].checked) {
+      return selectNumber[i].value;
+    }
+  }
+}
+
+const getDatafromServer = ev => {
+  ev.preventDefault();
+  const cardNumber = numberCard();
+
+  const url = `https://raw.githubusercontent.com/Adalab/cards-data/master/${cardNumber}.json`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      data = formatData(data);
+      saveDataIncardImage(data);
+      paintCard();
+      localStorage.setItem("numberSelected", cardNumber);
+    });
+};
+
+const formatData = data => {
+  const searchResult = [];
+  for (let i = 0; i < data.length; i = i + 1) {
+    searchResult.push({
+      image: data[i].image
+    });
+  }
+  console.log(
+    "Format JSON data and return it as array >>> Return",
+    searchResult
+  );
+  return searchResult;
+};
+
+const saveDataIncardImage = data => {
+  cardImage = data;
+  console.log("Save data in cardImage array >> cardImage", cardImage);
+};
+
+//cardImage[cardIndex].image(para que salgan los pokemon)
+const paintCard = () => {
+  const cardList = document.querySelector(".showCards__list");
+  let htmlCode = "";
+  for (let cardIndex = 0; cardIndex < cardImage.length; cardIndex++) {
+    htmlCode += `<li class="cards__list-card js-card">
+    <img  class="card__img js-card-img " src="${urlImage}">
+    <img class="card__img js-card-img hidden"
+    src="${cardImage[cardIndex].image}"></li>`;
+  }
+  cardList.innerHTML = htmlCode;
+  //pruebas
+  const cardsArray = document.querySelectorAll(".js-card");
+  for (const item of cardsArray) {
+    item.addEventListener("click", cardClicked);
+  }
+};
+
+//cambio de carta
+function cardClicked(event) {
+  const clickedImage = event.currentTarget.querySelectorAll(".js-card-img");
+  for (const item of clickedImage) {
+    item.classList.toggle("hidden");
+  }
+}
+
+btnStart.addEventListener("click", getDatafromServer);
+
+function getLevelFromLocalStorage() {
+  const numberSelected = localStorage.getItem("numberSelected");
+  const radioToSelect = document.querySelector(
+    `input[type="radio"][value="${numberSelected}"]`
+  );
+  radioToSelect.checked = true;
+}
+getLevelFromLocalStorage();
